35ab52ad4119e15184df222d1489288e60c64d0d
Matt Manley <mmanley29@hotmail.com>
2022-02-15 09:47:03 -0700

    Finished Snake (Unit05)

---
+++ Unit05/Game/Scripting/MoveActorsAction.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using Unit05.Game.Casting;
+
+
+namespace Unit05.Game.Scripting
+{
+    // TODO: Implement the MoveActorsAction class here
+
+    // 1) Add the class declaration. Use the following class comment. Make sure you
+    //    inherit from the Action class.
+
+    /// <summary>
+    /// <para>An update action that moves all the actors.</para>
+    /// <para>
+    /// The responsibility of MoveActorsAction is to move all the actors.
+    /// </para>
+    /// </summary>
+
+    // 2) Create the class constructor. Use the following method comment.
+
+    /// <summary>
+    /// Constructs a new instance of MoveActorsAction.
+    /// </summary>
+
+    // 3) Override the Execute(Cast cast, Script script) method. Use the following 
+    //    method comment. You custom implementation should do the following:
+    //    a) get all the actors from the cast
+    //    b) loop through all the actors
+    //    c) call the MoveNext() method on each actor.
+
+}
