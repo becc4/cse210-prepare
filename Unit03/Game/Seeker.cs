018252a22e6221f756916dd200d5314feba8cbc2
Becca Roeth <roe21002@byui.edu>
2022-10-11 22:01:24 -0600

    Added to Seeker File/Class

--- Unit03/Game/Seeker.cs
+++ Unit03/Game/Seeker.cs
@@ -13,14 +13,18 @@ namespace Unit03.Game
         /// The responsibility of a Seeker is to keep track of its location.
         /// </para>
         /// </summary>
-
+    public class Seeker {
+        private int _location = 0;
 
     // 2) Create the class constructor. Use the following method comment.
         
         /// <summary>
         /// Constructs a new instance of Seeker.
         /// </summary>
-       
+        //public Seeker() {
+        //    Random random = new Random();
+        //    _location = random.Next(1001);
+        //}
 
     // 3) Create the GetLocation() method. Use the following method comment.
         
@@ -28,6 +32,9 @@ namespace Unit03.Game
         /// Gets the current location.
         /// </summary>
         /// <returns>The current location as an int.</returns>
+        public int GetLocation() {
+            return _location;
+        }
         
 
     // 4) Create the MoveLocation(int location) method. Use the following method comment.
@@ -36,5 +43,8 @@ namespace Unit03.Game
         /// Moves to the given location.
         /// </summary>
         /// <param name="location">The given location.</param>
-    
+        public void MoveLocation(int location) {
+            this._location = location;
+        }
+    }
 }
