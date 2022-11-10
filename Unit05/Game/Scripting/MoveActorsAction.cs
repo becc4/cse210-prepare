6bbfbd210cc4c02cadbb10ff4574ecf2f662f2d5
Becca Roeth <roe21002@byui.edu>
2022-11-10 15:05:18 -0700

    Wrote on MoveActorsAction function

--- Unit05/Game/Scripting/MoveActorsAction.cs
+++ Unit05/Game/Scripting/MoveActorsAction.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using Unit05.Game.Casting;
+using Unit05.Game.Services;
 
 
 namespace Unit05.Game.Scripting
@@ -15,18 +16,26 @@ namespace Unit05.Game.Scripting
     /// The responsibility of MoveActorsAction is to move all the actors.
     /// </para>
     /// </summary>
-    public interface MoveActorsAction : Action {
+    public class MoveActorsAction : Action {
 
     // 2) Create the class constructor. Use the following method comment.
 
-    /// <summary>
-    /// Constructs a new instance of MoveActorsAction.
-    /// </summary>
+        /// <summary>
+        /// Constructs a new instance of MoveActorsAction.
+        /// </summary>
+        public MoveActorsAction() { 
+        }
 
-    // 3) Override the Execute(Cast cast, Script script) method. Use the following 
-    //    method comment. You custom implementation should do the following:
-    //    a) get all the actors from the cast
-    //    b) loop through all the actors
-    //    c) call the MoveNext() method on each actor.
+        // 3) Override the Execute(Cast cast, Script script) method. Use the following 
+        //    method comment. You custom implementation should do the following:
+        //    a) get all the actors from the cast
+        //    b) loop through all the actors
+        //    c) call the MoveNext() method on each actor.
+        public void Execute(Cast cast, Script script) {
+            List<Actor> actors = cast.GetAllActors();
+            foreach (Actor actor in actors) {
+                actor.MoveNext();
+            }
+        }
 
 }}
